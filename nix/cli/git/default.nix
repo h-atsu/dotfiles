9f097ccc83d379e07aaac888b80c24aa870a23a5
h-atsu <jhuim08@gmail.com>
2026-05-17 19:35:09 +0900

    [add] git設定を拡充

--- nix/cli/git/default.nix
+++ nix/cli/git/default.nix
@@ -2,4 +2,23 @@
   environment.systemPackages = [
     pkgs.git
   ];
+
+  home-manager.users.atsu = {
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+      maintenance.enable = true;
+
+      includes = [
+        {
+          path = "./conf.d/gitconfig";
+        }
+      ];
+    };
+
+    xdg.configFile."git/conf.d/" = {
+      source = ./conf.d;
+      recursive = true;
+    };
+  };
 }
