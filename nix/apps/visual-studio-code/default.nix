3a473c2a3beaa9dfc6ca1ea3f3259a585712cf4b
h-atsu <jhuim08@gmail.com>
2026-05-17 23:34:53 +0900

    [add] vscodeの設定を追加

--- nix/apps/visual-studio-code/default.nix
+++ nix/apps/visual-studio-code/default.nix
@@ -1,5 +1,31 @@
-{ ... }: {
+{ pkgs, ... }: {
   homebrew.casks = [
     "visual-studio-code"
   ];
+
+  home-manager.users.atsu = {
+    programs.vscode = {
+      enable = true;
+      package = null;
+      mutableExtensionsDir = true;
+
+      profiles.default = {
+        userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+        keybindings = builtins.fromJSON (builtins.readFile ./keybindings.json);
+        globalSnippets = builtins.fromJSON (builtins.readFile ./snippets/global.code-snippets);
+
+        extensions = with pkgs.vscode-extensions; [
+          charliermarsh.ruff
+          ms-python.python
+          ms-python.vscode-pylance
+          rust-lang.rust-analyzer
+          gruntfuggly.todo-tree
+          pkief.material-icon-theme
+          dracula-theme.theme-dracula
+          tuttieee.emacs-mcx
+          ms-vscode-remote.remote-ssh
+        ];
+      };
+    };
+  };
 }
