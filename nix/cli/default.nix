a3f1622cd3369ba31cf1c995cc4d56ee4697e593
h-atsu <jhuim08@gmail.com>
2026-06-04 23:42:53 +0900

    [add] fd, riggrepを導入

--- nix/cli/default.nix
+++ nix/cli/default.nix
@@ -4,12 +4,14 @@ _: {
     ./claude-code
     ./codex
     ./eza
+    ./fd
     ./git
     ./github-cli
     ./google-cloud-sdk
     ./htop
     ./mise
     ./pre-commit
+    ./ripgrep
     ./starship
     ./tree
     ./trash
